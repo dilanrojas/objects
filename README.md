62e741a12d3703423ecd74be772b914fb18b6137
Dilan Rojas <rojasvargasdilan010@gmail.com>
2026-02-15 12:03:42 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tarea III | Desarrollo de Software II
+
+## Objetivo
+
+Fortalecer la comprensión del uso de objetos en JavaScript mediante la
+lectura analítica del capítulo 4 del libro JavaScript for kids, la resolución de ejercicios
+prácticos y la reflexión escrita sobre los aprendizajes adquiridos.
+
+## Preview
---
+++ exercise_1/main.js
@@ -0,0 +1,17 @@
+var scores = {
+  dilan: 0,
+  jeremy: 0,
+  manfred: 0,
+  sonia: 0,
+}
+
+function decreaseScore(user) {
+  if (scores[user] == 0) return;
+  scores[user]--;
+  document.getElementById(`${user}-score`).textContent = scores[user];
+}
+
+function increaseScore(user) {
+  scores[user]++;
+  document.getElementById(`${user}-score`).textContent = scores[user];
+}
